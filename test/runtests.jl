52e0c0200572910b84a16b1b775a0ddf463403b2
Martin Kunz <martinkunz@email.cz>
2023-10-12 18:43:48 +0200

    fix(test): Aqua.jl ambiguities for v"1.1" and higher

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Aqua
 
 @testset "SIMParameterEstimation.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(SIMParameterEstimation)
+        Aqua.test_all(SIMParameterEstimation;  ambiguities=VERSION >= v"1.1")
     end
     # Write your tests here.
 end
