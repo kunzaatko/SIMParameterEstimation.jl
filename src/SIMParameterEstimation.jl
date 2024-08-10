b3155b95ac828390d26a57241c12318262f1cb30
Martin Kunz <martinkunz@email.cz>
2024-08-10 10:29:13 +0200

    chore: Update compat and restructure the package

--- src/SIMParameterEstimation.jl
+++ src/SIMParameterEstimation.jl
@@ -1,5 +1,30 @@
 module SIMParameterEstimation
 
-include("harmonic.jl")
+using SIMIlluminationPatterns
+using SIMIlluminationPatterns: IlluminationPattern
+# using SIMIlluminationPatterns.IlluminationPatterns: ParameterEstimator, PE
+# using StructuredIlluminationMicroscopy.Utils
+
+using Distributed
+
+abstract type ParameterEstimator{IP<:IlluminationPattern} end
+const PE = ParameterEstimator
+
+include("utils.jl")
+include("estimators/peak_frequency.jl")
+include("estimators/autocorrelation_frequency.jl")
+include("estimators/peak_phase_shift.jl")
+include("estimators/cross_correlation_phase_shift.jl")
+include("estimators/IP_cross_correlation_phase_offset.jl")
+include("estimators/cross_correlation_modulation.jl")
+
+export estimate
+
+export AutoCorrelationThroughInputInterpolation, ACTII
+export AutoCorrelationThroughOutputInterpolation, ACTOI
+export PeakPhaseShift, PPS
+export CrossCorrelationPhaseShift, CCPS
+export IPCrossCorrelationPhaseOffset, IPCCPO
+export CrossCorrelationModulation, CCM
 
 end
